abb95f27cd6248a1d0e6fe4a2d2540e1d06f2162
Andy M <andy@andypants.xyz>
2018-08-16 19:35:47 -0700

    initial game logic

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -10,10 +10,11 @@
   firebase.initializeApp(config);
 
 var database = firebase.database();
-var rpsRef = database.ref("/RPS")
+var rpsRef = database.ref("/RPS/game")
 var messagesRef = database.ref("/RPS/messages");
 var connectionsRef = database.ref("/RPS/connections");
-var username;
+var username, uid, opponentSpot, currentPlayers;
+var playerSpot = -1;
 
 $(function() {
 
@@ -24,13 +25,17 @@ $(function() {
         $("#username").val("");
 
         var connect = connectionsRef.push({username: username});
-
+        uid = connect.key;
         connect.onDisconnect().remove();
 
         $("#login-display").removeClass("show");
         $("#login-display").addClass("hide");
         $("#game").removeClass("hide");
         $("#game").addClass("show");
+
+        if(currentPlayers){
+            checkIfPlaying();
+        }
     })
 
     connectionsRef.on("value", function(snap){
@@ -51,7 +56,10 @@ $(function() {
         if(snap.val()){
             snapKeys = Object.keys(snap.val());
             if(snapKeys.length === 2){
-                console.log("Let's play!");
+
+                currentPlayers = snapKeys;
+                checkIfPlaying();
+
             }
         }
     })
@@ -59,26 +67,89 @@ $(function() {
     messagesRef.on("child_added", function(snapshot) {
    
         var chatItem = $("<p>");
-    
         chatItem.text(snapshot.val().message);
-
         $("#chat-display").append(chatItem);
         
-    
     });
+
+    rpsRef.on("value", function (snap) {
+        if (playerSpot === 0 || playerSpot === 1) {
+            var opponentChoice = snap.val()[opponentSpot].choice;
+            var playerChoice = snap.val()[playerSpot].choice;
+            if (opponentChoice != "none" && playerChoice != "none") {
+                if ((playerChoice === "rock" && opponentChoice === "scissors") ||
+                (playerChoice === "paper" && opponentChoice === "rock") ||
+                (playerChoice === "scissors" && opponentChoice === "paper")){
+                    console.log("You Win!")
+                }
+                else if ((opponentChoice === "rock" && playerChoice === "scissors") ||
+                        (opponentChoice === "paper" && playerChoice === "rock") ||
+                        (opponentChoice === "scissors" && playerChoice === "paper")){
+                    console.log("You Lose!");
+                }
+                else{
+                    console.log("You Tied!");
+                }
+            }
+        }
+    })
 
     $("#chat-submit").on("click", function(e){
         e.preventDefault();
-    
         var message = username + ": " + $("#chat-input").val();
         $("#chat-input").val("");
-        
         messagesRef.push({
           message: message
         })
-      
-      
      
     });
+
+    function showRPS(){
+        $("#display").empty();
+        var choices = $("<div>").addClass("row");
+        var rockCol = $("<div>").addClass("col-4");
+        var rock = $("<img>").attr("src", "assets/images/rock.png").attr("alt", "rock");
+        rockCol.append(rock);
+        var paperCol = $("<div>").addClass("col-4");
+        var paper = $("<img>").attr("src", "assets/images/paper.png").attr("alt", "paper");
+        paperCol.append(paper);
+        var scissorsCol = $("<div>").addClass("col-4");
+        var scissors = $("<img>").attr("src", "assets/images/scissors.png").attr("alt", "scissors");
+        scissorsCol.append(scissors);
+
+        choices.append(rockCol);
+        choices.append(paperCol);
+        choices.append(scissorsCol);
+        $("#display").append(choices);
+    }
+
+    $("#display").on("click", "img", function(e){
+        database.ref("/RPS/game/" + playerSpot).set({
+            choice: $(this).attr("alt")
+        })
+        $("#display").empty();
+        $("#display").append($("<h3>").text("Waiting on opponent..."));
+
+    })
+
+    function checkIfPlaying(){
+        playerSpot = currentPlayers.indexOf(uid);
+        if(playerSpot === 0){
+            opponentSpot = 1;
+        }
+        else{
+            opponentSpot = 0;
+        }
+        if(playerSpot != -1){
+            database.ref("/RPS/game/" + playerSpot).set({
+                choice: "none",
+            })
+            showRPS();
+        }
+        else{
+            $("#display").empty();
+            $("#display").append($("<h3>").text("Other players currently playing, please wait..."));
+        }
+    }
 
 })
